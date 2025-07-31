d91581a4af1276ef26493ff200d230e0bc860c0e
Carl <e12341573@student.tuwien.ac.at>
2025-07-31 16:29:42 +0200

    Proof of concept for ica.se/recept

--- recipes/views/newScraper.py
+++ recipes/views/newScraper.py
@@ -1,18 +1,35 @@
 from playwright.sync_api import sync_playwright
+import re
 
+recipe_pattern = re.compile(r"^https://www\.ica\.se/recept/.+-\d{6}/$")
 
+def find_all_recipe_links():
+    """
+    Finds all recipe links on the current page.
+    Returns a list of links that match the recipe pattern.
+    """
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto("https://www.ica.se/recept/korv/stroganoff")  # fixed URL
+        hrefs = page.eval_on_selector_all("a", "elements => elements.map(e => e.href)")
+        links = [href for href in hrefs if recipe_pattern.match(href)]
+        browser.close()
+        return links
 
-with sync_playwright() as p:
-
-    browser = p.chromium.launch()
-
-    page = browser.new_page()
-
-    page.goto("https://ica.se/recept/korv/stroganoff")
-
-    # Print all links on the page
-    links = page.query_selector_all('a')
-    for link in links:
-        print(link.get_attribute('href'))
-
-    browser.close()
+def find_ingridients_from_recipe(url):
+    """
+    Scrapes ingredients from a recipe page.
+    Returns a list of ingredients found on the page.
+    """
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        ingredients = page.eval_on_selector_all("div.ingredients-list-group__card", 
+            "elements => elements.map(e => e.textContent.trim())")
+        browser.close()
+        return ingredients
+    
+recipelist = find_all_recipe_links()
+print(find_ingridients_from_recipe(recipelist[0]))
